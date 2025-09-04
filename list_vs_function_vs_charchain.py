dc19d4d3405f768ed41b653d1494c052d3cb2468
Guillaume Postic <gui.postic@gmail.com>
2025-09-04 08:02:11 +0200

    add py file

---
+++ list_vs_function_vs_charchain.py
@@ -0,0 +1,20 @@
+
+def try_to_modify_list(input_list):
+    input_list.append('lol')
+
+mylist = ['A', 'B', 'C']
+try_to_modify_list(mylist)
+print(mylist)
+
+# CONCLUSION 1: no need for a return in the function
+
+# ------------------------------------------------------------------------------
+
+def try_to_modify_charchain(input_charchain):
+    input_charchain + 'lol'
+
+mycharchain = 'ABCD'
+try_to_modify_charchain(mycharchain)
+print(mycharchain)
+
+# CONCLUSION 2: need for a return in the function
